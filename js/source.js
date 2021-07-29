53b63e6f9cd19770c4bb4c588064f136bec50960
Feeloh <philipmwendwa3@gmail.com>
2021-07-29 22:01:52 +0300

    Code restructure

--- js/source.js
+++ js/source.js
@@ -1,14 +1,14 @@
 function searchCity() {
-    let pop = '';
+    let population = '';
     const searchCity = document.getElementById('city').value;
     let http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let response = JSON.parse(this.responseText);
-            const city = response.city.name + '</br>';
-            pop += city + 'Population: ' + response.city.population;
+            const city = response?.city?.name + '</br>';
+            population += city + 'Population: ' + response.city.population;
             document.getElementById('main').innerHTML = city;
-            document.getElementById('main').innerHTML = pop;
+            document.getElementById('main').innerHTML = population;
             const weeklyData = response.list;
             let display = '';
             let weekly = '';
@@ -43,7 +43,6 @@ function searchCity() {
 }
 
 function kenyaCityForecast(j) {
-    document.getElementById('city').value = '';
     let cities = [
         'nairobi',
         'nanyuki',
@@ -52,56 +51,75 @@ function kenyaCityForecast(j) {
         'nakuru',
         'marsabit'
     ];
-    let pop = '';
+    let population = '';
     let count = 0;
-    let rand = Math.floor(Math.random() * (7 - 0) + 0);
+    let randomNumber = Math.floor(Math.random() * 7);
+
     if (typeof j === 'undefined') {
-        i = 0;
+        cityIndex = 0;
     } else {
-        i = rand + j;
+        cityIndex = randomNumber + j;
     }
-    for (i; i < cities.length; i++) {
+    let citiesData = [];
+    
+    for (cityIndex; cityIndex < cities.length; cityIndex++) {
+
+        const citiesForecast = {};
         let http = new XMLHttpRequest();
+
         http.onreadystatechange = function () {
+
             if (this.readyState == 4 && this.status == 200) {
+
                 let response = JSON.parse(this.responseText);
-                let city = response.city.name + '</br>';
-                pop += city + 'Population: ' + response.city.population;
-                const todayWeather = response.list;
-                let display = '';
-                let weekly = '';
-                if (count == 0) {
-                    document.getElementById('main').innerHTML = city;
-                    document.getElementById('main').innerHTML = pop;
-                    for (let p = 0; p < todayWeather.length; p++) {
-                        if (todayWeather[p].dt_txt.substring(11) === '15:00:00') {
-                            let timestamp = todayWeather[p].dt;
-                            const a = new Date(timestamp * 1000);
-                            const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
-                                'Saturday'
-                            ];
-                            const dayOfWeek = days[a.getDay()];
-                            const weeklyWeather = todayWeather[p].weather[0].main;
-                            const weeklyTemp = todayWeather[p].main.feels_like;
-                            weekly += dayOfWeek + "</br>" + "Forecast: " + weeklyWeather + "</br>" +
-                                "Temp: " + weeklyTemp + "</br></br>";
-                        }
+                let city = response?.city?.name + '</br>';
+
+                population += city + 'Population: ' + response?.city?.population;
+                const todayWeather = response?.list;
+
+                citiesForecast.name = response?.city?.name;
+                citiesForecast.population = response?.city?.population;
+
+                let todaysWeatherDisplay = '';
+                let weeklysWeatherDisplay = '';
+
+                for (let p = 0; p < todayWeather.length; p++) {
+
+                    if (todayWeather[p].dt_txt.substring(11) === '15:00:00') {
+                        
+                        let timestamp = todayWeather[p].dt;
+                        const a = new Date(timestamp * 1000);
+                        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+                            'Saturday'
+                        ];
+                        const dayOfWeek = days[a.getDay()];
+                        const weeklyWeather = todayWeather[p]?.weather[0]?.main;
+                        const weeklyTemp = todayWeather[p].main.feels_like;
+                        weeklysWeatherDisplay += dayOfWeek + "</br>" + "Forecast: " + weeklyWeather + "</br>" +
+                            "Temp: " + weeklyTemp + "</br></br>";
                     }
-                    document.getElementById('weeklyWeather').innerHTML = weekly;
-                    for (let k = 0; k < 7; k++) {
-                        const time = todayWeather[k].dt_txt.substring(11);
-                        const mainForecast = todayWeather[k].weather[0].main;
-                        const mainTemp = todayWeather[k].main.temp;
-                        display += time + " " + mainForecast + " " + mainTemp +
-                            '<span>°c</span></br></br></br>';
-                    }
-                    document.getElementById('todayWeather').innerHTML = display;
+
                 }
+
+                for (let k = 0; k < 7; k++) {
+                    const time = todayWeather[k].dt_txt.substring(11);
+                    const mainForecast = todayWeather[k].weather[0].main;
+                    const mainTemp = todayWeather[k].main.temp;
+                    todaysWeatherDisplay += time + " " + mainForecast + " " + mainTemp +
+                        '<span>°c</span></br></br></br>';
+                }
+                    
+                citiesForecast.weekly = weeklysWeatherDisplay;
+                citiesForecast.todayWeather = todaysWeatherDisplay;
                 count++;
             }
         }
-        http.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q=" + cities[i] +
+        http.open("GET", "http://api.openweathermap.org/data/2.5/forecast?q=" + cities[cityIndex] +
             ",ke&APPID=83416958429879bd5fb4ee5dad8da3fe&units=metric", true);
         http.send();
+
+        citiesData.push(citiesForecast);
     }
+
+    console.log(citiesData, 'my object');
 }
